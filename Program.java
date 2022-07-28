4c2824edb80207cb4b4e846ee27cfef4c315fe14
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-28 13:57:08 +0300

    upd

--- Program.java
+++ Program.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Program {
+import chat.Chat;
+import chat.ChatMsgOper;
+import users.Holder;
+import users.RegularUser;
+
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) throws IOException {
+        Holder bob = new Holder("Bob");
+        RegularUser dave = new RegularUser("Dave");
+        Chat chat = new Chat(bob);
+        ChatMsgOper msgOper = new ChatMsgOper(chat);
+        bob.addInChat(dave);
+//        chat.showMembers(chat.members);
+//        msgOper.sendTextMsg(bob.fillMessage(), bob);
+//        msgOper.readTextMessages();
+//        Saver save = new Saver("Saves//nSave.odt");
+//        save.fillSaveFile();
+//        Loader load = new Loader("Saves//Save.txt");
+//        load.loadSave();
+
+
+    }
+
+
 }
