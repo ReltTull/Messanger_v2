ca82c20a23ac76e9ade105a40a380c9755abc3b8
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-28 15:57:07 +0300

    comments added

--- chat/Chat.java
+++ chat/Chat.java
@@ -6,14 +6,24 @@ import java.util.ArrayList;
 
 public class Chat {
     User holder;
-    public static ArrayList<User> members = new ArrayList<>();
+    // Проблема: без static массивы не отображают добавленных юзеров.
+    static public ArrayList<User> members = new ArrayList<>();
     static public ArrayList<User> admins = new ArrayList<>();
     static public ArrayList<User> blockedUsers = new ArrayList<>();
+
+    /**
+     * Создаю чат, сразу добавляя в него создателя. Проблема: создатель выводится в members два раза.
+     * @param user
+     */
     public Chat(User user) {
         this.holder = user;
         members.add(holder);
     }
 
+    /**
+     * Метод вывода юзеров из массивов. Чтобы не делать по методу на каждый массив.
+     * @param caste - массивы, которые надо вывести.
+     */
     public void showMembers (ArrayList<User> caste) {
         for (int i = 0; i < caste.size(); i++) {
             System.out.println(caste.get(i).name);
