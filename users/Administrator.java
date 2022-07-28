8424a2548c91f749a72425d02e2d379bb3893bc9
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-28 13:54:42 +0300

    admin functions added

--- users/Administrator.java
+++ users/Administrator.java
@@ -1,2 +1,28 @@
-package users;public class Administrator {
+package users;
+
+import chat.Chat;
+import message.TextMessage;
+
+public class Administrator extends User {
+    public Administrator(String name) {
+        super(name, "Administrator");
+    }
+
+    Chat chat;
+    @Override
+    public TextMessage fillMessage() {
+        return super.fillMessage();
+    }
+
+    public void addInChat(User user) {
+        if (!chat.blockedUsers.contains(user)) {
+            chat.members.add(user);
+            System.out.println(user.name + " has been added to chat.");
+        }
+    }
+    public void blockMember(User user) {
+        chat.blockedUsers.add(user);
+        chat.members.remove(user);
+        System.out.println(user.name + " has been blocked.");
+    }
 }
