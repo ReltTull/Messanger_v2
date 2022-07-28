86ed037118d6f7e05c0378b7e9f22b35640835c4
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-28 13:53:24 +0300

    save function added

--- saveload/Saver.java
+++ saveload/Saver.java
@@ -1,2 +1,33 @@
-package saveload;public class Saver {
+package saveload;
+
+import chat.FilePath;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Saver {
+    String path;
+    File save;
+
+
+    public Saver(String path) throws IOException {
+        this.path = path;
+        this.save = new File(path);
+        save.createNewFile();
+    }
+
+    FilePath filePath = new FilePath();
+
+    public void fillSaveFile() throws IOException {
+        FileReader reader = new FileReader(filePath.actualFile);
+        Scanner scan = new Scanner(reader);
+        FileWriter writer = new FileWriter(save);
+        while (scan.hasNext()) {
+            writer.write(scan.nextLine());
+        }
+        writer.flush();
+        writer.close();
+    }
 }
