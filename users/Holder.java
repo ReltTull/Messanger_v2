fe18524d6a8139e1628283df01703b7a15aad605
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-28 13:55:08 +0300

    holder functions added

--- users/Holder.java
+++ users/Holder.java
@@ -1,2 +1,39 @@
-package users;public class Holder {
+package users;
+
+import chat.Chat;
+import message.TextMessage;
+
+public class Holder extends User{
+    public Holder(String name) {
+        super(name, "Chat holder");
+    }
+
+    @Override
+    public TextMessage fillMessage() {
+        return super.fillMessage();
+    }
+    Chat chat = new Chat(Holder.this);
+    void addAdmin(User candidate) {
+        if (chat.members.contains(candidate)) {
+            chat.admins.add(candidate);
+            System.out.println(candidate.name + " is administrator.");
+        }
+    }
+
+    public void removeAdmin(User user) {
+        if (chat.admins.contains(user)) {
+            chat.admins.remove(user);
+            System.out.println(user.name + " was removed from administrators.");
+        }
+    }
+
+    public void addInChat(User user) {
+            chat.members.add(user);
+    }
+    public void blockMember(User user) {
+        chat.blockedUsers.add(user);
+        chat.members.remove(user);
+        System.out.println(user.name + " has been blocked.");
+    }
+
 }
