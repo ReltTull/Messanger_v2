9f496e850e123fc71697149d31ad0e629410164f
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-28 13:56:02 +0300

    abstract user role added

--- users/User.java
+++ users/User.java
@@ -3,19 +3,26 @@ package users;
 import message.TextMessage;
 
 import java.util.Scanner;
+import chat.ChatMsgOper;
+import saveload.Saver;
 
-abstract class User {
-    String name;
+public abstract class User {
+    public String name;
+    public String role;
 
-    public User(String name) {
+    public User(String name, String role) {
         this.name = name;
+        this.role = role;
     }
 
-    void fillMessage() {
+    TextMessage fillMessage() {
         System.out.println("Enter your message: ");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         TextMessage tm = new TextMessage(text);
+        return tm;
+
+
     }
 
 
